98132bebfc9e171fa1b14f8ddfed63baecc479a6
fiantonumber1 <87689030+fiantonumber1@users.noreply.github.com>
2022-06-24 10:12:52 +0700

    Update
    Update_datasheet

---
+++ download_datsheet.py
@@ -0,0 +1,7 @@
+import httplib2
+
+username = 'daicwozuser'
+password = 'dA1c_U$3rW0zz'
+for i in range(436,493):
+  url = f"http://'daicwozuser':'dA1c_U$3rW0zz'@dcapswoz.ict.usc.edu/wwwdaicwoz//{i}_P.zip"
+  connection = httplib2.HTTPSConnectionWithTimeout(url)
